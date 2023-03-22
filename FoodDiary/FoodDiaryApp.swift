1d2095799cae9810bc5c9b1ce1ba97448e1ba8be
k-chui <kevin.kchui@gmail.com>
2023-03-22 11:29:31 -0700

    Initial Commit

---
+++ FoodDiary/FoodDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDiaryApp.swift
+//  FoodDiary
+//
+//  Created by Kevin Chui on 2023-03-22.
+//
+
+import SwiftUI
+
+@main
+struct FoodDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
